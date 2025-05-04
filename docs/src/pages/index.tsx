dcb68402be70fcd64898b814ae9c04b8932ecd39
thelvl13mage <thelvl13mage@gmail.com>
2025-05-04 22:59:54 +0200

    Adds better social card

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -56,8 +56,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} Documentation`}
+      description="<Translate id='homepage.header.subtitle' />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
