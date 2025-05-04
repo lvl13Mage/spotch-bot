c5455d1109c8ca392e6b33c149030bdaedefad4e
thelvl13mage <thelvl13mage@gmail.com>
2025-05-04 23:50:23 +0200

    Try fix initial color mode

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -17,14 +17,11 @@ function HomepageHeader() {
     // wait for the color mode to be available
     return null;
   }
-  console.log('colorMode', colorMode);
 
   const logoSrc = isDarkMode
     ? require('@site/static/img/logo-transparent-purple-small.png').default
     : require('@site/static/img/logo-transparent-white-small.png').default;
 
-
-  console.log('logoSrc', logoSrc);
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
